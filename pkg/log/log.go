38412799e62a5bc9265eb5d520d3cfd50e0243a6
Robin Jarry <robin@jarry.cc>
2026-06-08 01:01:00 +0200

    go/log: add leveled logging with syslog support
    Add a logging package with debug, info, notice, warning, error
    and fatal levels. Default to stderr/stdout with timestamps and
    switch to syslog when InitSyslog() is called, as expected by a
    system daemon.
    
    Signed-off-by: Robin Jarry <robin@jarry.cc>

---
+++ pkg/log/log.go
@@ -0,0 +1,77 @@
+// Patchwork - automated patch tracking system
+// Copyright (C) 2026 Robin Jarry <robin@jarry.cc>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package log
+
+import (
+	"fmt"
+	"log"
+	"log/syslog"
+	"os"
+	"path"
+)
+
+const flags = log.Lshortfile | log.Ldate | log.Ltime
+
+var (
+	dbg    = log.New(os.Stdout, "DEBUG ", flags)
+	notice = log.New(os.Stdout, "NOTICE ", flags)
+	info   = log.New(os.Stdout, "INFO ", flags)
+	warn   = log.New(os.Stderr, "WARN ", flags)
+	err    = log.New(os.Stderr, "ERROR ", flags)
+)
+
+func newSyslog(tag string, prio syslog.Priority) *log.Logger {
+	w, e := syslog.New(prio, tag)
+	if e != nil {
+		log.Fatalf("syslog.New: %v", e)
+	}
+	return log.New(w, "", log.Lshortfile)
+}
+
+func InitSyslog() {
+	tag := path.Base(os.Args[0])
+	dbg = newSyslog(tag, syslog.LOG_DEBUG)
+	notice = newSyslog(tag, syslog.LOG_NOTICE)
+	info = newSyslog(tag, syslog.LOG_INFO)
+	warn = newSyslog(tag, syslog.LOG_WARNING)
+	err = newSyslog(tag, syslog.LOG_ERR)
+}
+
+func ErrLogger() *log.Logger {
+	return err
+}
+
+func logfmt(message string, args ...any) string {
+	if len(args) > 0 {
+		message = fmt.Sprintf(message, args...)
+	}
+	return message
+}
+
+func Debugf(message string, args ...any) {
+	dbg.Output(2, logfmt(message, args...))
+}
+
+func Infof(message string, args ...any) {
+	info.Output(2, logfmt(message, args...))
+}
+
+func Noticef(message string, args ...any) {
+	notice.Output(2, logfmt(message, args...))
+}
+
+func Warnf(message string, args ...any) {
+	warn.Output(2, logfmt(message, args...))
+}
+
+func Errorf(message string, args ...any) {
+	err.Output(2, logfmt(message, args...))
+}
+
+func Fatalf(message string, args ...any) {
+	err.Output(2, logfmt(message, args...))
+	os.Exit(1)
+}
